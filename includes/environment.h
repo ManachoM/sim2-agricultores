b820bd697b1d805d9172138032fb894c6caf8117
Manacho <manuel.manriquez.b@usach.cl>
2023-04-08 16:37:44 -0400

    Environment class implementation

---
+++ includes/environment.h
@@ -0,0 +1,88 @@
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+#include "agent.h"
+// TODO: Pasar a objeto de configuración
+#define AFECTACION_HELDAS 0.3
+#define AFECTACION_SEQUIAS 0.3
+#define AFECTACION_OLAS_CALOR 0.1
+#define AFECTACION_PLAGAS 0.3
+
+#include "consumidor.h"
+#include "event.h"
+#include "fel.h"
+#include "feria.h"
+#include "glob.h"
+
+class Consumidor;
+class Feria;
+class Feriante;
+class Agricultor;
+class Producto;
+class Monitor;
+
+enum EVENTOS_AMBIENTE
+{
+  INICIO_FERIA,
+  FIN_FERIA,
+  CALCULO_PRECIOS,
+  LIMPIEZA_MERCADO_MAYORISTA
+};
+
+class Environment
+{
+private:
+  FEL *fel;
+  std::map<int, Agricultor *> agricultores;
+  std::map<int, Feria *> ferias;
+  std::map<int, Consumidor *> consumidores;
+  std::map<int, Feriante *> feriantes;
+  std::map<int, Producto *> productos;
+  std::map<int, std::vector<Consumidor *>> consumidor_dia;
+  std::map<int, std::vector<Producto *>> venta_producto_mes;
+  // TODO:  helada_nivel;
+  std::vector<int> sequias_nivel;
+  std::vector<int> oc_nivel;
+  Monitor *monitor;
+
+public:
+  explicit Environment(FEL *_fel);
+
+  void set_feriantes(std::map<int, Feriante *> _feriantes);
+
+  void set_consumidores(std::map<int, Consumidor *> _cons);
+
+  void set_ferias(std::map<int, Feria *> _ferias);
+
+  void process_event(Event *e);
+
+  short get_day_week();
+
+  short get_day_month();
+
+  short get_month();
+
+  short get_year();
+
+  std::map<int, Feria *> get_ferias();
+
+  std::map<int, Feriante *> get_feriantes();
+
+  std::map<int, Agricultor *> get_agricultores();
+
+  std::map<int, Producto *> get_productos();
+
+  std::map<int, std::vector<Producto *>> get_venta_producto_mes();
+
+  int  get_nivel_heladas();
+
+  int get_nivel_sequias();
+
+  int  get_nivel_olas_calor();
+
+  void  initialize_system();
+
+  ~Environment();
+};
+
+#endif // !ENVIRONMENT_H
---
+++ src/environment.cpp
@@ -0,0 +1,64 @@
+#include "../includes/environment.h"
+
+Environment::Environment(FEL *_fel) { this->fel = _fel; }
+
+void Environment::set_feriantes(std::map<int, Feriante *> _feriantes)
+{
+  this->feriantes = _feriantes;
+}
+
+void Environment::set_consumidores(std::map<int, Consumidor *> _cons)
+{
+  this->consumidores = _cons;
+}
+
+void Environment::set_ferias(std::map<int, Feria *> _ferias)
+{
+  this->ferias = _ferias;
+}
+
+void Environment::process_event(Event *e)
+{
+  printf("Procesando evento del ambiente...");
+}
+
+short Environment::get_day_week()
+{
+  return (short)(this->fel->get_time() / 24) % 24;
+}
+
+short Environment::get_day_month()
+{
+  return (short)(this->fel->get_time() / 24.0) % 30;
+}
+
+short Environment::get_month()
+{
+  return (short)(this->fel->get_time() / 720) % 12;
+}
+
+short Environment::get_year()
+{
+  return ((short)this->fel->get_time() / 8640);
+}
+
+std::map<int, Feria *> Environment::get_ferias() { return this->ferias; }
+
+std::map<int, Feriante *> Environment::get_feriantes() { return this->feriantes; }
+
+std::map<int, Agricultor *> Environment::get_agricultores() { return this->agricultores; }
+
+std::map<int, Producto *> Environment::get_productos() { return this->productos; }
+
+std::map<int, std::vector<Producto *>> Environment::get_venta_producto_mes() { return this->venta_producto_mes; }
+
+int Environment::get_nivel_heladas() { return -1; }
+
+int Environment::get_nivel_sequias() { return this->sequias_nivel.at(this->get_month()); }
+
+int Environment::get_nivel_olas_calor() { return this->oc_nivel.at(this->get_month()); }
+
+void Environment::initialize_system()
+{
+  printf("Inicializando sistema...\n");
+}

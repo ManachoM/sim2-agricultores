6126de8864671cde8af1623eadc416139f53354e
Manacho <manuel.manriquez.b@usach.cl>
2023-04-09 20:35:50 -0400

    Added product class

---
+++ includes/product.h
@@ -0,0 +1,79 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#include "glob.h"
+
+enum UNIT_TYPES
+{
+    KILOS,
+    UNIDAD
+};
+
+class Producto
+{
+private:
+    static int _current_id;
+    int id;
+    std::string const nombre;
+    std::vector<int> const meses_siembra;
+    std::vector<int> const meses_venta;
+    double dias_cosecha;
+    short unidad;
+    double rendimiento;
+    double volumen_feriante;
+    double volumen_un_consumidor;
+    double prob_consumo;
+    std::vector<int> heladas;
+    std::vector<int> sequias;
+    std::vector<int> olas_calor;
+    std::vector<int> plagas;
+
+public:
+    Producto(std::string _nombre,
+             std::vector<int> _siembra,
+             std::vector<int> _venta,
+             double _cosecha,
+             short _unit,
+             double _rendimiento,
+             double _vol_feriante,
+             double _vol_cons,
+             double _prob_cons);
+
+    std::string get_nombre() const;
+
+    short get_unidad() const;
+
+    double get_rendimiento() const;
+
+    double get_volumen_feriante() const;
+
+    double get_volumen_consumidor() const;
+
+    double get_probabilidad_consumo() const;
+
+    std::vector<int> get_meses_siembra() const;
+
+    std::vector<int> get_meses_venta() const;
+
+    int get_id() const;
+
+    double get_dias_cosecha() const;
+
+    void set_heladas(std::vector<int> _hel);
+
+    void set_sequias(std::vector<int> _seq);
+
+    void set_olas_calor(std::vector<int> _oc);
+
+    void set_plagas(std::vector<int> _pl);
+
+    std::vector<int> get_heladas();
+
+    std::vector<int> get_sequias();
+
+    std::vector<int> get_olas_calor();
+
+    std::vector<int> get_plagas();
+};
+
+#endif // !PRODUCT_H
---
+++ src/product.cpp
@@ -0,0 +1,44 @@
+#include "../includes/product.h"
+
+int Producto::_current_id(-1);
+
+Producto::Producto(
+    std::string const _nombre,
+    std::vector<int> const _siembra,
+    std::vector<int> const _venta,
+    double _cosecha,
+    short _unit,
+    double _rendimiento,
+    double _vol_feriante,
+    double _vol_cons,
+    double _prob_cons)
+    : id(++_current_id),
+      nombre(_nombre),
+      meses_siembra(_siembra),
+      meses_venta(_venta),
+      dias_cosecha(_cosecha),
+      unidad(_unit),
+      rendimiento(_rendimiento),
+      volumen_feriante(_vol_feriante),
+      volumen_un_consumidor(_vol_cons),
+      prob_consumo(_prob_cons){};
+
+std::string Producto::get_nombre() const { return this->nombre; }
+
+short Producto::get_unidad() const { return this->unidad; }
+
+double Producto::get_rendimiento() const { return this->rendimiento; }
+
+double Producto::get_volumen_feriante() const { return this->volumen_feriante; }
+
+double Producto::get_volumen_consumidor() const { return this->volumen_un_consumidor; }
+
+double Producto::get_probabilidad_consumo() const { return this->prob_consumo; }
+
+std::vector<int> Producto::get_meses_siembra() const { return this->meses_siembra; }
+
+std::vector<int> Producto::get_meses_venta() const { return this->meses_venta; }
+
+int Producto::get_id() const { return this->id; }
+
+double Producto::get_dias_cosecha() const { return this->dias_cosecha; }

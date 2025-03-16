141f3c7efd5e463a7f1b3d367d3111c6a7b3032e
Manacho <manuel.manriquez.b@usach.cl>
2025-03-16 01:49:52 -0300

    Actualizados los inputs con nuevo archivo de producto

--- src/feriante_estacional_proporcional.cpp
+++ src/feriante_estacional_proporcional.cpp
@@ -3,36 +3,31 @@
 #include "../includes/environment.h"
 #include "../includes/product.h"
 
-FerianteEstacionalProporcional::FerianteEstacionalProporcional (
+FerianteEstacionalProporcional::FerianteEstacionalProporcional(
     Environment *_env, FEL *fel, MercadoMayorista *mer, int feria_id,
-    int prod_amount)
-    : FerianteEstatico (_env, fel, mer, feria_id), prod_amount (prod_amount)
-{
-}
+    int prod_amount
+)
+    : FerianteEstatico(_env, fel, mer, feria_id), prod_amount(prod_amount) {}
 
-std::vector<int>
-FerianteEstacionalProporcional::choose_product ()
-{
+std::vector<int> FerianteEstacionalProporcional::choose_product() {
   std::vector<int> prods_seleccionados;
 
-  std::vector<Producto *> prods = this->env->get_venta_producto_mes ()
-                                      .find (this->env->get_month ())
-                                      ->second;
+  std::vector<Producto *> prods =
+      this->env->get_venta_producto_mes().find(this->env->get_month())->second;
 
   std::random_device rd;
-  std::mt19937 gen (rd ());
+  std::mt19937 gen(rd());
 
-  std::uniform_real_distribution<double> d (0.0, 1.0);
+  std::uniform_real_distribution<double> d(0.0, 1.0);
 
-  for (auto prod : prods)
-    {
-      if (d (gen) < prod->get_probabilidad_consumo ())
-        continue;
-      prods_seleccionados.push_back (prod->get_id ());
+  for (auto prod : prods) {
+    if (d(gen) < prod->get_probabilidad_consumo())
+      continue;
+    prods_seleccionados.push_back(prod->get_id());
 
-      if ((int)prods_seleccionados.size () >= this->prod_amount)
-        break;
-    }
+    if ((int)prods_seleccionados.size() >= this->prod_amount)
+      break;
+  }
 
   return prods_seleccionados;
 }
